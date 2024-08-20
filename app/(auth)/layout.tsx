f9717fd4e63356dcf11b050e4d18832566c6cc2c
leighTOR <leighinna.jamolin@lspu.edu.ph>
2024-08-20 14:46:52 +0800

    fix some clerk errors

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
 
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Inter } from "next/font/google"
 import '../globals.css'
 
 export const metadata = {
@@ -10,17 +10,22 @@ export const metadata = {
 
 const inter = Inter({ subsets: ["latin"] })
 
-export default function RootLayout({
-    children }: {
-        children: React.ReactNode
-    }) {
-        return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1`}>
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
-        )
-}
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <ClerkProvider>
+        <html lang="en">
+          <body>
+            <header>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            <main>{children}</main>
+          </body>
+        </html>
+      </ClerkProvider>
+    )
+  }
