72eed68b64fc8d47d749b8959a36fb889677bf45
leighTOR <leighinna.jamolin@lspu.edu.ph>
2024-08-20 22:04:35 +0800

    ongoing topbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
+'use client'
+
+import { ClerkProvider, UserButton } from '@clerk/nextjs'
+
+const DotIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
+      <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+    </svg>
+  )
+}
+
 export default function Home() {
   return (
-    <>
-      <h1 className="head-text text-left">Home</h1>
-    </>
+    <ClerkProvider>
+      <header>
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Action
+            label="Open chat"
+            labelIcon={<DotIcon />}
+            onClick={() => alert('init chat')}
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+    </header>
+    </ClerkProvider>
+    
   )
 }
--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,5 +1,48 @@
+import {
+  ClerkProvider,
+  OrganizationSwitcher,
+  SignedIn,
+  SignOutButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
 function TopBar() {
-    return <h1>TopBar</h1>
+  return (
+    <ClerkProvider>
+      <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+          <OrganizationSwitcher
+            appearance={{
+              elements: {
+                organizationSwitcherTrigger: "py-2 px-4",
+              },
+            }}
+          />
+      </div>
+    </nav>
+    </ClerkProvider>
+    
+  );
 }
 
 export default TopBar;
