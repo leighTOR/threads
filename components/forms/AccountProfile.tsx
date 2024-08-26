d162ec680152508f62e970010a3164c610c0fe0a
leighTOR <leighinna.jamolin@lspu.edu.ph>
2024-08-26 13:19:33 +0800

    started onboarding page

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { useForm } from 'react-hook-form';
+import { Form } from "../ui/form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { UserValidation } from '@/lib/validations/user';
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            username: '',
+            bio: ''
+        }
+    });
+    return (
+        <Form>
+
+        </Form>
+    )
+}
+
+export default AccountProfile;
